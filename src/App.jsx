5fd36b949b6bcc4bb7b8124941bcc8f706218d23
dennisshtavica <dennisshtavica@gmail.com>
2023-09-12 22:21:55 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route, Navigate, Outlet} from "react-router-dom"
+
+import StartPage from "./pages/StartPage"
+import SignUp from "./pages/Users/SignUp"
+
+
 function App() {
   return (
-    <>
-
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
